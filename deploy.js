824d036e69074d7aa4a096b7a96e924c49db68ee
Todo <stodosan@gmail.com>
2017-11-13 22:08:53 +0200

    Me using deploy script

---
+++ deploy.js
@@ -0,0 +1,31 @@
+const git = require('simple-git');
+const USER = 'something';
+
+const fs = require('fs');
+fs.readFile('./credentials.json', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  deployToGit(JSON.parse(data));
+});
+
+function randomId() {
+  return "Me using deploy script";
+}
+
+function deployToGit(cred) {
+  git().add('.')
+    .commit(randomId())
+    .push(['-u', 'origin', 'master'],
+          () => console.log('Pushed~'),
+          (err) => console.error('Failed:', err)
+      );
+}
+
+// const git = require('simple-git/promise');
+// const remote = `https://${USER}:${PASS}@${REPO}`;
+//
+// git().silent(true)
+//   .clone(remote)
+//   .then(() => console.log('finished'))
+//   .catch((err) => console.error('failed: ', err));
