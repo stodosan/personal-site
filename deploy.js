f4c5fbf3f51dbfa711d65a347d93908f9c530d1c
Todo <stodosan@gmail.com>
2017-11-13 22:12:42 +0200

    Me using deploy script: 2017-11-13 22:12:41

--- deploy.js
+++ deploy.js
@@ -10,7 +10,7 @@ fs.readFile('./credentials.json', 'utf8', function (err,data) {
 });
 
 function randomId() {
-  return "Me using deploy script: " + (new Date().toString());
+  return "Me using deploy script: " + (new Date().toLocaleString());
 }
 
 function deployToGit(cred) {
